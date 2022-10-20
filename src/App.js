3265e8b16945f9c11785ad29ad256e91d4b1c3ae
hjkwon0814 <hjkwon0814@naver.com>
2022-10-20 21:39:46 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button"
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={"안녕하세요"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
