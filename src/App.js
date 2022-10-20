f0c314b0f347d78ae3246785457a3a93a120bb28
hjkwon0814 <hjkwon0814@naver.com>
2022-10-20 21:59:02 +0900

    useEffect when value changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import Button from "./Button"
-import styles from "./App.module.css";
+
 import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API.....");
+  }, [])
+  useEffect(() => {
+      console.log("I run when keyword is changed");
+  }, [keyword])
+  useEffect(() => {
+      console.log("I run when counter is changed");
+  }, [counter])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search"></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
