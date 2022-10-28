a0b16b5cebbcfd8974a9da02231be5353dba1edd
hjkwon <hjkwon0814@naver.com>
2022-10-28 22:38:48 +0900

    recap Link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ function Movie({id, coverImg, title, summary, genres}) {
         <img src={coverImg} alt={title}></img>
         <h2><Link to="/movie/${id}">{title}</Link></h2>
         <p>{summary}</p>
+        
         <ul>
             {genres.map((g) => (
                 <li key={g}>{g}</li>
