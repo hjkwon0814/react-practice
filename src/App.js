15fcbd41aba0b588cb6e34ec02b5b1420b60ac6a
hjkwon0814 <hjkwon0814@naver.com>
2023-02-25 21:58:15 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-
-import { useState, useEffect } from "react";
-
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("CALL THE API.....");
-  }, [])
-  useEffect(() => {
-      console.log("I run when keyword is changed");
-  }, [keyword])
-  useEffect(() => {
-      console.log("I run when counter is changed");
-  }, [counter])
-  return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search"></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      </div>
-      )
-}
-=======
 import {
   BrowserRouter as Router,
   Routes,
@@ -43,4 +15,3 @@ function App() {
 };
 
 export default App;
->>>>>>> bc7f02ea249735399bb5873b2057dc3a5c4c5cf9
